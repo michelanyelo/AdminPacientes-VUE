1c9226a984a853e876e8aadad86fd0cb309a4c0f
MUNI <miguelmonzon.inf@gmail.com>
2024-10-07 13:23:19 -0300

    Added FormularioComp to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import HeaderComp from './components/HeaderComp.vue';
+import FormularioComp from './components/FormularioComp.vue'
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
     <HeaderComp />
+    <div class="mt-12 md:flex">
+      <FormularioComp />
+    </div>
   </div>
 </template>
 
